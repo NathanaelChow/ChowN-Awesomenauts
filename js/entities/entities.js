f74d778faf32dfae32bd7ca016322f16041b43c1
NathanaelChow <nhchowder@gmail.com>
2015-03-09 10:47:06 -0700

    To video 10
    animation

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,1 +1,38 @@
-// TODO
+game.PlayerEntity=me.Entity.extend({
+   init: function(x, y, settings){
+     this._super(me.Entity, 'init', [x, y, {
+             image: "player",
+             width: 64,
+             height: 64,
+             spritewidth:"64",
+             spriteheight:"64",
+             getShape: function(){
+                 return(new me.Rect(0, 0, 64, 64)).toPolygon();
+             }
+     }]);
+     
+     this.body.setVelocity(5,20);
+     
+     this.renderable.addAnimation("idle", [14]);
+     this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+     
+     this.renderable.setCurrentAnimation("idle");
+     
+ 
+   },
+   
+   update: function(delta){
+       if(me.input.isKeyPressed("right")){
+           //adds to the positionn of my x by the velocity defined above in
+           //setVelocity() and mulitiplying it be me.timer.tick.
+           //me.timer.tick makes the movement look smooth
+           this.body.vel.x += this.body.accel.x * me.timer.tick;
+       }else{
+           this.body.vel.x = 0;
+       }
+       
+       this.body.update(delta);
+       return true;
+   }
+    
+});
--- js/resources.js
+++ js/resources.js
@@ -1,28 +1,31 @@
 game.resources = [
+    /* Graphics. 
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
 
-	/* Graphics. 
-	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
-	 */
+    {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    {name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png"},
+    {name: "player", type: "image", src: "data/img/theplayer.png"},
+    /* Atlases 
+     * @example
+     * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+     */
 
-	/* Atlases 
-	 * @example
-	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
-	 */
-		
-	/* Maps. 
-	 * @example
-	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
-	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
- 	 */
+    /* Maps. 
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
 
-	/* Background music. 
-	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
-	 */	
+    {name: "level01", type: "tmx", src: "data/map/test.tmx"},
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
 
-	/* Sound effects. 
-	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
-	 */
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
 ];
