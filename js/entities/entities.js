e78920494912fde5d7a8ad29116b7909c02150bd
NathanaelChow <nhchowder@gmail.com>
2015-03-10 09:20:35 -0700

    Video 12

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -2,19 +2,20 @@ game.PlayerEntity=me.Entity.extend({
    init: function(x, y, settings){
      this._super(me.Entity, 'init', [x, y, {
              image: "player",
-             width: 64,
-             height: 64,
-             spritewidth:"64",
-             spriteheight:"64",
+             width: 63,
+             height: 63,
+             spritewidth:"63",
+             spriteheight:"63",
              getShape: function(){
-                 return(new me.Rect(0, 0, 64, 64)).toPolygon();
+                 return(new me.Rect(0, 0, 20, 45)).toPolygon();
              }
      }]);
      
      this.body.setVelocity(5,20);
+     me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
      
-     this.renderable.addAnimation("idle", [14]);
-     this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+     this.renderable.addAnimation("idle", [0]);
+     this.renderable.addAnimation("walk", [1,2,3,4,5,6,7,8], 80);
      
      this.renderable.setCurrentAnimation("idle");
      
@@ -22,17 +23,126 @@ game.PlayerEntity=me.Entity.extend({
    },
    
    update: function(delta){
-       if(me.input.isKeyPressed("right")){
-           //adds to the positionn of my x by the velocity defined above in
-           //setVelocity() and mulitiplying it be me.timer.tick.
-           //me.timer.tick makes the movement look smooth
+       
+        if(me.input.isKeyPressed("right")){
            this.body.vel.x += this.body.accel.x * me.timer.tick;
+           this.flipX(false);
        }else{
            this.body.vel.x = 0;
        }
        
+        if (me.input.isKeyPressed("left")) {
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+            this.flipX(true);
+
+        } else {
+            this.body.vel.x - 0;
+      
+        }
+       
+               if (me.input.isKeyPressed('jump')) {   
+    if (!this.body.jumping && !this.body.falling) {
+        // set current vel to the maximum defined value
+        // gravity will then do the rest
+        this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+        // set the jumping flag
+        this.body.jumping = true;
+        // play some audio 
+        me.audio.play("jump");
+    }
+}
+       
+       
+       
+        if(this.body.vel.x !== 0){
+        if (!this.renderable.isCurrentAnimation("walk")) {
+                this.renderable.setCurrentAnimation("walk");
+                
+        }   
+    }else{
+        this.renderable.setCurrentAnimation("idle");
+    }
+        
+       
        this.body.update(delta);
+       
+       this._super(me.Entity,"update", [delta])
+       
        return true;
    }
+    
+});
+
+game.PlayerBaseEntity=me.Entity.extend({
+    init : function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, {
+                image: "tower",
+                width:100,
+                height:100,
+                spritewidth: "100",
+                spriteheight: "100",
+                getShape: function(){
+                    return(new me.Rect(0, 0, 100, 100)).toPolygon();
+                }
+            }]);
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "PlayerBaseEntity";
+        
+    },
+    
+    update:function(delta){
+        if(this.health<=0){
+            this.broken = true;
+        }   
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
+        
+    }
+    
+});
+
+game.EnemyBaseEntity=me.Entity.extend({
+    init : function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, {
+                image: "tower",
+                width:100,
+                height:100,
+                spritewidth: "100",
+                spriteheight: "100",
+                getShape: function(){
+                    return(new me.Rect(0, 0, 100, 100)).toPolygon();
+                }
+            }]);
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "EnemyBaseEntity";
+        
+    },
+    
+    update:function(delta){
+        if(this.health<=0){
+            this.broken = true;
+        }   
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
+        
+    }
     
 });
